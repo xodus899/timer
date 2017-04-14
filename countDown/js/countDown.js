ab8995339b0c1aaaf32381f2a0cbf73656c57464
Christopher Justice <justcd@mail.broward.edu>
2017-04-14 09:48:42 -0400

    test

--- countDown/js/countDown.js
+++ countDown/js/countDown.js
@@ -2,8 +2,7 @@ $(document).ready(function(){
 	
 
 	$(".countDown").countdown({
-		// until: $.countdown.UTCDate(-4, 2017, 6, 15, 17, 0, 0)
-		until: $.countdown.UTCDate(+10, 2018,  7 - 1, 23)
+		until: $.countdown.UTCDate(-4, 2017, 6, 15, 17, 0, 0)
 
 	});
 
