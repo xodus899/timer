98251d057d3d4d5b9928efee3adb094b2317113d
Christopher Justice <justcd@mail.broward.edu>
2017-07-09 22:10:47 -0400

    update

--- countDown/index.php
+++ countDown/index.php
@@ -1,1 +1,30 @@
-<?php header( 'Location: /index.html' ) ;  ?>
+
+
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width" >
+        <title>G8 Extravaganza</title>
+        <link rel="stylesheet" type="text/css" href="countdown.css">
+        <meta http-equiv="refresh" content="1500" />
+    </head>
+    <body>
+    <img id="mouseImage" src="pontiac_logo.gif" alt="G8"/>
+    <div id="extravaganza">
+        <div class="heading">
+        <h1> Countdown to the</h1>
+        <h2> G8 Extravaganza! </h2>
+
+        </div>
+        <div class="countDown">
+
+
+        </div>
+    </div>
+    <script type="text/javascript" src="js/jquery-2.1.3.min.js"> </script>
+    <script type="text/javascript" src="js/jquery.plugin.min.js"> </script>
+    <script type="text/javascript" src="js/jquery.countdown.min.js"> </script>
+    <script src="js/countDown.js"> </script>
+    </body>
+</html>
